57fff6f54eefc1b7dd865e0bd60b5e1f88081fad
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-03-14 17:28:32 +0400

    KE-47: Add PeopleViewController

--- kodeapp/kodeapp/Modules/People/Views/PeopleViewController.swift
+++ kodeapp/kodeapp/Modules/People/Views/PeopleViewController.swift
@@ -8,6 +8,102 @@
 import UIKit
 
 final class PeopleViewController: UIViewController {
-    
+
+    // MARK: - Subviews
+    private let headerSearchBar = SearchBar()
+    private let tableView = PeopleTableView()
+
+    // MARK: - View model
+
+    private let viewModel: PeopleViewViewModel
+
+    // MARK: - Init
+
+    init(viewModel: PeopleViewViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAppearance()
+        setupSearchBar()
+        addSubviews()
+        setupLayout()
+    }
+
+    // MARK: - UI constants
+
+    private enum UIConstants {
+        static let searchBarHorizontalOffset: CGFloat = 16
+    }
 }
 
+// MARK: - Setup appearance
+
+private extension PeopleViewController {
+
+    func setupAppearance() {
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = true
+    }
+}
+
+// MARK: - Setup subviews
+
+private extension PeopleViewController {
+
+    func addSubviews() {
+        view.addSubviews([
+            headerSearchBar,
+            tableView
+        ])
+    }
+
+    func setupLayout() {
+        NSLayoutConstraint.activate([
+            headerSearchBar.topAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.topAnchor
+            ),
+            headerSearchBar.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: UIConstants.searchBarHorizontalOffset
+            ),
+            headerSearchBar.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: -UIConstants.searchBarHorizontalOffset
+            )
+        ])
+        //        tableView.pinToEdges(view)
+    }
+}
+
+// MARK: - UISearchBarDelegate
+
+extension PeopleViewController: UISearchBarDelegate {
+
+    func setupSearchBar() {
+        headerSearchBar.delegate = self
+    }
+
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        headerSearchBar.isSearching = true
+        headerSearchBar.showCancelButton()
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        headerSearchBar.isSearching = false
+        headerSearchBar.hideCancelButton()
+        headerSearchBar.didEndEditing()
+    }
+
+    func searchBarBookmarkButtonClicked(_ searchBar: UISearchBar) {
+        headerSearchBar.isFilterClicked = !headerSearchBar.isFilterClicked
+    }
+}
