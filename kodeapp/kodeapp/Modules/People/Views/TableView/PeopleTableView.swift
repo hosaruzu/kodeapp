67cdfd034c5f6e8b61209ca96adb72ac18fafda3
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-03-14 17:27:41 +0400

    KE-47: Add blank table view and cell classes

--- kodeapp/kodeapp/Modules/People/Views/TableView/PeopleTableView.swift
+++ kodeapp/kodeapp/Modules/People/Views/TableView/PeopleTableView.swift
@@ -9,4 +9,31 @@ import UIKit
 
 final class PeopleTableView: UIView {
 
+    // MARK: - Subviews
+    private let tableView = UITableView()
+
+    // MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        add()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func add() {
+        addSubviews([tableView])
+        tableView.backgroundColor = .systemBackground
+    }
+
+    func setupConstaints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
 }
