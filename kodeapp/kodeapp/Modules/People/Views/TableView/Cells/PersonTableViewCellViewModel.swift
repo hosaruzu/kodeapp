e51a1432c5f6939fe2266016d9cad021a3dc3b79
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-03-15 20:15:27 +0400

    KE-47: Create Person Cell and Cell View Model

---
+++ kodeapp/kodeapp/Modules/People/Views/TableView/Cells/PersonTableViewCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  PersonTableViewCellViewModel.swift
+//  kodeapp
+//
+//  Created by Artem Tebenkov on 15.03.2024.
+//
+
+import Foundation
+
+struct PersonTableViewCellViewModel {
+    let name: String
+    let role: String
+    let tag: String
+}
