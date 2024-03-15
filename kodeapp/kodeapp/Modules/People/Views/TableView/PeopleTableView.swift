6e6d026fd05e910054603a714440653a75dd930f
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-03-15 20:17:01 +0400

    KE-47: Setup TableView with People Cell and Mocks

--- kodeapp/kodeapp/Modules/People/Views/TableView/PeopleTableView.swift
+++ kodeapp/kodeapp/Modules/People/Views/TableView/PeopleTableView.swift
@@ -12,20 +12,47 @@ final class PeopleTableView: UIView {
     // MARK: - Subviews
     private let tableView = UITableView()
 
+    // MARK: - DEBUG
+
+    private let data: [PersonTableViewCellViewModel] = [
+        .init(name: "Person", role: "role", tag: "pr"),
+        .init(name: "Person", role: "role", tag: "pr"),
+        .init(name: "Person", role: "role", tag: "pr"),
+        .init(name: "Person", role: "role", tag: "pr")
+    ]
+
     // MARK: - Init
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        add()
+        setupTableView()
+        setupLayout()
+        setupConstaints()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
 
-    func add() {
+// MARK: - Setup TableView
+
+private extension PeopleTableView {
+
+    func setupTableView() {
+        tableView.separatorStyle = .none
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(PersonTableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+}
+
+// MARK: - Setup layout
+
+private extension PeopleTableView {
+
+    func setupLayout() {
         addSubviews([tableView])
-        tableView.backgroundColor = .systemBackground
     }
 
     func setupConstaints() {
@@ -35,5 +62,23 @@ final class PeopleTableView: UIView {
             tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension PeopleTableView: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        data.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? PersonTableViewCell else { fatalError() }
+        cell.setup(with: data[indexPath.row])
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
