24022edddb7e16e7509c11e0e51895c5d16fa23c
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-03-19 18:13:52 +0400

    KE-59: Create People flow coordinator

---
+++ kodeapp/kodeapp/Core/Assembly/PeopleCoordinator.swift
@@ -0,0 +1,49 @@
+//
+//  PeopleCoordinator.swift
+//  kodeapp
+//
+//  Created by Artem Tebenkov on 19.03.2024.
+//
+
+import Foundation
+
+final class PeopleCoordinator: BaseCoordinator {
+
+    // MARK: - Callback
+
+    var finishFlow: (() -> Void)?
+
+    // MARK: - Dependencies
+
+    private let screenFactory: ScreenFactory
+    private let router: Router
+
+    // MARK: - Init
+
+    init(
+        screenFactory: ScreenFactory,
+        router: Router
+    ) {
+        self.screenFactory = screenFactory
+        self.router = router
+    }
+
+    // MARK: - Coordinator
+
+    override func start() {
+        showPeople()
+    }
+
+    // MARK: - Flow
+
+    private func showPeople() {
+        let peopleScreen = screenFactory.makePeopleScreen()
+//        peopleScreen.onSelectPerson = { [weak self] in self?.showPerson() }
+        router.setRootModule(peopleScreen, hideBar: true)
+    }
+
+    private func showPerson() {
+        // Person page
+        // router.push(person page)
+    }
+}
