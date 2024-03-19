d2f98288509dd75a67e974e2af6ca68fd3734c04
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-03-19 18:13:28 +0400

    KE-59: Create App Coordinator

---
+++ kodeapp/kodeapp/Core/Assembly/AppCoordinator.swift
@@ -0,0 +1,44 @@
+//
+//  AppCoordinator.swift
+//  kodeapp
+//
+//  Created by Artem Tebenkov on 19.03.2024.
+//
+
+import Foundation
+
+final class AppCoordinator: BaseCoordinator {
+
+    // MARK: - Dependencies
+
+    private let coordinatorFactory: CoordinatorFactory
+    private let router: Router
+
+    // MARK: - Init
+
+    init(
+        coordinatorFactory: CoordinatorFactory,
+        router: Router
+    ) {
+        self.coordinatorFactory = coordinatorFactory
+        self.router = router
+    }
+
+    // MARK: - Coordinator
+
+    override func start() {
+        runPeopleFlow()
+    }
+
+    // MARK: - Flows
+
+    private func runPeopleFlow() {
+        let coordinator = coordinatorFactory.makePeopleCoordinator(router: router)
+        coordinator.finishFlow = { [weak self, weak coordinator] in
+            self?.start()
+            self?.removeDependency(coordinator)
+        }
+        self.addDependency(coordinator)
+        coordinator.start()
+    }
+}
