0ebed03d365188ad7ce8833bd7319609f5a85c6d
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-03-25 11:43:53 +0400

    Create person model

---
+++ kodeapp/kodeapp/Core/Models/Person.swift
@@ -0,0 +1,24 @@
+//
+//  Person.swift
+//  kodeapp
+//
+//  Created by Artem Tebenkov on 25.03.2024.
+//
+
+import Foundation
+
+struct PeopleResponse: Decodable {
+    let items: [Person]
+}
+
+struct Person: Decodable {
+    let id: String
+    let firstName: String
+    let lastname: String
+    let userTag: String
+    let department: String
+    let position: String
+    let birthday: String
+    let phone: String
+}
+
