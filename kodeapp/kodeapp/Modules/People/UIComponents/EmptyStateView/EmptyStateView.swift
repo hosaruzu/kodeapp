e2f5774f690f08a1e0256313ad0124fece01b96b
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-05-31 12:14:51 +0400

    Create Empty state view UI

---
+++ kodeapp/kodeapp/Modules/People/UIComponents/EmptyStateView/EmptyStateView.swift
@@ -0,0 +1,78 @@
+//
+//  EmptyStateView.swift
+//  kodeapp
+//
+//  Created by Artem Tebenkov on 31.05.2024.
+//
+
+import UIKit
+
+final class EmptyStateView: UIView {
+
+    // MARK: - Subviews
+
+    private let emptyImageView: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFit
+        image.image = .init(systemName: "magnifyingglass.circle.fill")
+        return image
+    }()
+
+    private let emptyTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 17, weight: .semibold)
+        label.textColor = .label
+        label.text = "We didn't find anyone"
+        return label
+    }()
+
+    private let emptySubtitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        label.textColor = .secondaryLabel
+        label.text = "Try adjusting the query"
+        return label
+    }()
+
+    // MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupVStack()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Setup layout
+
+private extension EmptyStateView {
+
+    func setupVStack() {
+        let vStack = UIStackView(arrangedSubviews: [emptyImageView, emptyTitleLabel, emptySubtitleLabel])
+        vStack.axis = .vertical
+        vStack.spacing = Spec.vStackDefaultSpacing
+        vStack.setCustomSpacing(Spec.titleToSubtitleSpacing, after: emptyTitleLabel)
+        vStack.alignment = .center
+
+        addSubviews([vStack])
+        NSLayoutConstraint.activate([
+            emptyImageView.widthAnchor.constraint(equalToConstant: Spec.imageViewWidth),
+            emptyImageView.heightAnchor.constraint(equalToConstant: Spec.imageViewHeight),
+
+            vStack.topAnchor.constraint(equalTo: topAnchor),
+            vStack.leadingAnchor.constraint(equalTo: leadingAnchor),
+            vStack.trailingAnchor.constraint(equalTo: trailingAnchor),
+            vStack.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+}
+
+private enum Spec {
+    static let imageViewWidth: CGFloat = 56
+    static let imageViewHeight: CGFloat = 56
+    static let vStackDefaultSpacing: CGFloat = 8
+    static let titleToSubtitleSpacing: CGFloat = 12
+}
