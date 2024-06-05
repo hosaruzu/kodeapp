cfb59a5c3eb5ab97f732e4a6ec6d0de3a6d61eba
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-06-05 20:03:07 +0400

    Add Network monitor

---
+++ kodeapp/kodeapp/Core/Services/Network/NetworkMonitor.swift
@@ -0,0 +1,38 @@
+//
+//  NetworkMonitor.swift
+//  kodeapp
+//
+//  Created by Artem Tebenkov on 05.06.2024.
+//
+import Network
+
+protocol NetworkMonitor {
+    var onConnectionStatusChange: ((Bool) -> Void)? { get }
+}
+
+final class NetworkMonitorImpl: NetworkMonitor {
+
+    private let networkMonitor = NWPathMonitor()
+    private let workerQueue = DispatchQueue(label: "monitor.network")
+
+    var onConnectionStatusChange: ((Bool) -> Void)?
+
+    init() {
+        start()
+    }
+
+    func start() {
+        networkMonitor.pathUpdateHandler = { path in
+            Task {
+                await self.setStatus(path.status == .satisfied)
+            }
+        }
+        networkMonitor.start(queue: workerQueue)
+    }
+
+    func setStatus(_ status: Bool) async {
+        await MainActor.run {
+            self.onConnectionStatusChange?(status)
+        }
+    }
+}
